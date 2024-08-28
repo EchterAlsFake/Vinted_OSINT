ed023577be97080e50ecd64955b875851fa380ef
Johannes Habel <EchterAlsFake@proton.me>
2024-08-28 15:43:43 +0200

    - fixed a lot of things - code refactoring

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import json
 import argparse
 import os.path
+import time
+
 import requests
 
 from fake_useragent import UserAgent
@@ -26,23 +28,30 @@ from prettytable import PrettyTable
 
 init(autoreset=True)
 
-parser = argparse.ArgumentParser(prog="Vinted OSINT", description="An Open-Source intelligent Tool to get information"
-                                                                  "about User/s on Vinted")
-parser.add_argument("-u" , "--username", help="Username", required=False)
-parser.add_argument("-ul", "--user_list", help="List of user names, separated by new line", required=False)
-parser.add_argument("-e", "--extension", help="The website extension e,g .fr .com .de", required=False)
-parser.add_argument("-c", "--license", help="Licensing and copyright information", required=False,
-                    action="store_true")
-parser.add_argument("-a", "--fetch_all", help="Fetches literally ALL information", required=False)
-parser.add_argument("--no_export", help="If enabled, won't export any data", required=False,
-                    action="store_true")
-parser.add_argument("-f", "--export_format", help="Defines the export format [json,csv]", required=False)
+parser = argparse.ArgumentParser(
+    prog="Vinted OSINT",
+    description="An Open-Source intelligent Tool to get information about User/s on Vinted"
+)
 
+# Mutually exclusive group for username or user list
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-u", "--username", help="Username")
+group.add_argument("-ul", "--user_list", help="List of user names, separated by new line")
 
+# Additional arguments
+parser.add_argument("-e", "--extension", help="The website extension e.g. .fr .com .de", default=".com")
+parser.add_argument("-c", "--license", help="Licensing and copyright information", action="store_true")
+parser.add_argument("-a", "--fetch_all", help="Fetches literally ALL information", action="store_true")
+parser.add_argument("--no_export", help="If enabled, won't export any data", action="store_true")
+parser.add_argument("-f", "--export_format", help="Defines the export format [json, csv, html, latex, txt]",
+                    choices=["json", "csv", "html", "latex", "txt"])
+
+# Parse arguments
 args = parser.parse_args()
 
+# Display license information
 if args.license:
-    print(f"""
+    print("""
 Vinted Osint - 2024
 Developed by Johannes Habel | EchterAlsFake
 Licensed under GPLv3
@@ -51,57 +60,48 @@ Used projects:
 - requests
 - colorama
 - prettytable
+- fake_useragent 
 
 Thanks to: https://github.com/herissondev/vinted-api-wrapper for the idea how to do the authentication stuff
-""")
+    """)
+    exit(0)
 
-if args.no_export:
-    export = False
+# Set default values based on arguments
+export = not args.no_export  # Export is enabled unless --no_export is set
+extension = args.extension
+username = args.username
+username_list = args.user_list
+fetch_all = args.fetch_all
 
-else:
-    export = True
+# Determine export format and handle warnings if necessary
+export_format = args.export_format if args.export_format else "json"
 
-if args.extension:
-    extension = args.extension
-
-else:
-    extension = ".com"
-
-username = None
-username_list = None
-
-if args.username:
-    username = args.username
-
-elif args.user_list:
-    username_list = args.user_list
-if args.export_format is True:
-    export_format = "json"
-
-else:
-    export_format = args.export_format
-
-if args.fetch_all:
-    fetch_all = True
-
-else:
-    fetch_all = False
-
-if args.export_format is None:
-    export = False
-    print(f"{Fore.LIGHTRED_EX}[!]{Fore.LIGHTYELLOW_EX}Warning: {Fore.LIGHTWHITE_EX}You did not set an export format, no"
-          f"data will be saved to a file.")
+if export and not args.export_format:
+    export = False  # Disable export if no format is provided
+    print(f"{Fore.LIGHTRED_EX}[!]{Fore.LIGHTYELLOW_EX}Warning: {Fore.LIGHTWHITE_EX}You did not set an export format, "
+          f"no data will be saved to a file.")
 
 
 class OSINT:
     def __init__(self, username, username_list):
         self.session = requests.Session()
         self.VINTED_AUTH_URL = f"https://www.vinted{extension}/auth/token_refresh"
+        self.dicts = {}
+
+        self.payment_table = PrettyTable()
+
+        self.main_table = PrettyTable()
+        self.main_table.field_names = ["Category", "Variable", "Value"]
+
+        self.photo_table = PrettyTable()
+        self.photo_table.field_names = ["Category", "Variable", "Value"]
+
+        self.discount_table = PrettyTable()
+        self.discount_table.field_names = ["Category", "Variable", "Value"]
+
 
         if not username is None:
-            _ = []
-            _.append(username)
-            self.usernames = _  # I know this is weird
+            self.usernames = [username]
 
         elif not username_list is None:
             with open(username_list, "r") as users:
@@ -124,8 +124,10 @@ class OSINT:
     def start(self):
         for username_ in self.usernames:
             self.dictionary = self.get_information(username_)
-            print(self.dictionary)
+            self.create_tables()
             self.print_everything()
+            self.dictionary.clear()
+
 
 
     def get_information(self, username):
@@ -140,42 +142,7 @@ class OSINT:
             else:
                 return json.loads(data.content.decode("utf-8"))["user"]
 
-    from prettytable import PrettyTable
-
-    # Function to add rows to the general table based on categories and variables
-    def add_rows(self, table, category, variables, dictionary):
-        for var in variables:
-            value = dictionary.get(var)
-            formatted_value = self.format_value(value)
-            table.add_row([category, var, formatted_value])
-
-
-    # Function to create and display the payment table if fetch_all is True
-    def create_payment_table(self, payment_methods):
-        payment_table = PrettyTable()
-        payment_table.field_names = ["ID", "Code", "Requires Credit Card", "Event Tracking Code", "Icon",
-                                     "Enabled", "Translated Name", "Note", "Method Change Possible"]
-
-        # Add rows to the payment table based on the payment methods
-        for payment in payment_methods:
-            payment_table.add_row([payment.get('id'), payment.get('code'), payment.get('requires_credit_card'),
-                                   payment.get('event_tracking_code'), payment.get('icon'), payment.get('enabled'),
-                                   payment.get('translated_name'), payment.get('note'),
-                                   payment.get('method_change_possible')])
-        return payment_table
-
-    def format_value(self, value):
-        """Format the value to handle None and nested data."""
-        if value is None:
-            return "N/A"
-        if isinstance(value, dict):
-            return "; ".join(f"{k}: {v}" for k, v in value.items())
-        return str(value)
-
-    # Main method to print everything
-    def print_everything(self):
-        table = PrettyTable()
-        table.field_names = ["Category", "Variable", "Value"]
+    def create_tables(self):
 
         categories = {
             "User Info": [
@@ -206,45 +173,161 @@ class OSINT:
                 "carrier_ids", "carriers_without_custom_ids", "updated_on", "msg_template_count",
                 "business_account", "business", "default_address", "code"
             ]
+        }  # Categories for the main table
+
+        for category, variables in categories.items():
+            self.add_rows(self.main_table, category, variables, self.dictionary)
+
+        photo_data = self.dictionary.get("photo", None)
+        if not photo_data is None:
+            self.photo_table = PrettyTable()
+            self.photo_table.field_names = ["Category", "Variable", "Value"]
+            self.photo_table.add_row(["Photo", "ID", photo_data.get("id", "N/A")])
+            self.photo_table.add_row(["Photo", "Width", photo_data.get("width", "N/A")])
+            self.photo_table.add_row(["Photo", "Height", photo_data.get("height", "N/A")])
+            self.photo_table.add_row(["Photo", "URL", photo_data.get("url", "N/A")])
+            self.photo_table.add_row(["Photo", "Dominant Color", photo_data.get("dominant_color", "N/A")])
+            self.photo_table.add_row(["Photo", "Dominant Color Opaque", photo_data.get("dominant_color_opaque", "N/A")])
+            self.photo_table.add_row(["Photo", "Is Suspicious", photo_data.get("is_suspicious", "N/A")])
+            self.photo_table.add_row(["Photo", "Full Size URL", photo_data.get("full_size_url", "N/A")])
+            self.photo_table.add_row(["Photo", "Is Hidden", photo_data.get("is_hidden", "N/A")])
+
+            # Extract thumbnails data
+            thumbnails = photo_data.get("thumbnails", [])
+            for thumbnail in thumbnails:
+                self.photo_table.add_row(
+                    ["Thumbnails", f"Type ({thumbnail.get('type', 'N/A')})", thumbnail.get("url", "N/A")])
+                self.photo_table.add_row(
+                    ["Thumbnails", f"Width ({thumbnail.get('type', 'N/A')})", thumbnail.get("width", "N/A")])
+                self.photo_table.add_row(
+                    ["Thumbnails", f"Height ({thumbnail.get('type', 'N/A')})", thumbnail.get("height", "N/A")])
+
+        bundle_discount = self.dictionary.get("bundle_discount", None)
+        if not bundle_discount is None:
+            self.discount_table.add_row(["Bundle Discount", "ID", bundle_discount.get("id", "N/A")])
+            self.discount_table.add_row(["Bundle Discount", "User ID", bundle_discount.get("user_id", "N/A")])
+            self.discount_table.add_row(["Bundle Discount", "Enabled", bundle_discount.get("enabled", "N/A")])
+            self.discount_table.add_row(
+                ["Bundle Discount", "Minimal Item Count", bundle_discount.get("minimal_item_count", "N/A")])
+
+            # Extract discounts data
+            discounts = bundle_discount.get("discounts", [])
+            for discount in discounts:
+                self.discount_table.add_row(["Discounts", f"Minimal Item Count ({discount.get('minimal_item_count', 'N/A')})",
+                               discount.get("fraction", "N/A")])
+
+        payment_methods = self.dictionary.get("accepted_pay_in_methods", None)
+        if payment_methods is not None:
+            self.create_payment_table(payment_methods)
+
+
+
+    def add_rows(self, table, category, variables, dictionary):
+        for var in variables:
+            value = dictionary.get(var)
+            formatted_value = self.format_value(var, value)
+            table.add_row([category, var, formatted_value])
+
+
+    # Function to create and display the payment table if fetch_all is True
+    def create_payment_table(self, payment_methods):
+        self.payment_table.field_names = ["ID", "Code", "Requires Credit Card", "Event Tracking Code", "Icon",
+                                     "Enabled", "Translated Name", "Note", "Method Change Possible"]
+
+        # Add rows to the payment table based on the payment methods
+        for payment in payment_methods:
+            self.payment_table.add_row([payment.get('id'), payment.get('code'), payment.get('requires_credit_card'),
+                                   payment.get('event_tracking_code'), payment.get('icon'), payment.get('enabled'),
+                                   payment.get('translated_name'), payment.get('note'),
+                                   payment.get('method_change_possible')])
+
+    def format_value(self, variable, value):
+        """Format the value to handle None and nested data."""
+        if value is None:
+            return "N/A"
+        if isinstance(value, dict):
+            self.dicts.update({variable: value})
+
+        else:
+            return str(value)
+
+    # Main method to print everything
+    def print_everything(self):
+        self.main_table.sortby = "Category"
+        self.discount_table.sortby = "Category"
+        self.photo_table.sortby = "Category"
+
+
+        print(self.main_table)
+        print(self.photo_table)
+        print(self.discount_table)
+        print(self.payment_table)
+
+        mapping_main = {
+            "json": self.main_table.get_json_string(),
+            "html": self.main_table.get_html_string(),
+            "csv": self.main_table.get_csv_string(),
+            "latex": self.main_table.get_latex_string(),
+            "txt": self.main_table.get_string()
         }
 
-        # Add rows to the general table based on the categories
-        for category, variables in categories.items():
-            self.add_rows(table, category, variables, self.dictionary)
+        mapping_photo = {
+            "json": self.photo_table.get_json_string(),
+            "html": self.photo_table.get_html_string(),
+            "csv": self.photo_table.get_csv_string(),
+            "latex": self.photo_table.get_latex_string(),
+            "txt": self.photo_table.get_string()
+        }
 
-        # Sort the general table by Category and Variable
-        table.sortby = "Category"
-        print(table)
+        mapping_discount = {
+            "json": self.discount_table.get_json_string(),
+            "csv": self.discount_table.get_csv_string(),
+            "latex": self.discount_table.get_latex_string(),
+            "txt": self.discount_table.get_string(),
+            "html": self.discount_table.get_html_string()
+        }
 
-        # Process and display the payment table if fetch_all is True
-        if fetch_all:
-            payment_methods = self.dictionary.get("accepted_pay_in_methods", [])
-            payment_table = self.create_payment_table(payment_methods)
-            print(payment_table)
+        mapping_payment = {
+            "json": self.payment_table.get_json_string(),
+            "csv": self.payment_table.get_csv_string(),
+            "latex": self.payment_table.get_latex_string(),
+            "txt": self.payment_table.get_string(),
+            "html": self.payment_table.get_html_string()
+        }
+
 
         if export:
-            # Determine the export format and process accordingly
-            if export_format == "json":
-                data_base = table.get_json_string()
-                data_payment = payment_table.get_json_string() if fetch_all else None
+            data = [
+                mapping_main.get(export_format),
+                mapping_photo.get(export_format),
+                mapping_discount.get(export_format),
+                mapping_payment.get(export_format),
+            ]
 
-            elif export_format == "csv":
-                data_base = table.get_csv_string()
-                data_payment = payment_table.get_csv_string() if fetch_all else None
+            for idx, data_ in enumerate(data):
+                if idx == 0:
+                    filename = f"data_main_{username}.{export_format}"
 
-            else:
-                data_base = None
-                data_payment = None
+                elif idx == 1:
+                    filename = f"data_photo_{username}.{export_format}"
+
+                elif idx == 2:
+                    filename = f"data_discount_{username}.{export_format}"
+
+                elif idx == 3:
+                    filename = f"data_payment_{username}.{export_format}"
+
+
+                with open(filename, "w") as file:
+                    file.write(data_)
+
+            print(f"{Fore.LIGHTMAGENTA_EX}Data was exported in: .{export_format}")
 
             if not os.path.exists(username):
                 os.mkdir(username)
 
             with open(f"{username}{os.sep}data.{export_format}", "w") as file:
-                file.write(str(data_base))
-
-            if fetch_all:
-                with open(f"{username}{os.sep}data_payment.{export_format}", "w") as file_2:
-                    file_2.write(str(data_payment))
+                file.write(str(data))
 
 
     def authentication_flow(self):
@@ -258,16 +341,3 @@ class OSINT:
             print(e)
 
 OSINT(username=username, username_list=username_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
